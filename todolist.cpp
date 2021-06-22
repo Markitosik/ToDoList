2bda940c8a0757b742a9765e11e2b7d101c2019d
Nalipo <86319018+Nalipo@users.noreply.github.com>
2021-06-22 21:53:10 +0300

    Add files via upload

--- todolist.cpp
+++ todolist.cpp
@@ -24,13 +24,22 @@ CToDoList::CToDoList()
     QHBoxLayout* pHLayoutLabels = new QHBoxLayout();
     pMainLayout->addLayout(pHLayoutLabels);
 
+    /*Задаем обозначение для поставленных задач*/
+    QLabel* plblPending = new QLabel("Выполнить", this);
+    plblPending->setStyleSheet("font-size: 15pt;");
+    pHLayoutLabels->addWidget(plblPending);
 
+    /*Задаем обозначение для выполненных задач*/
+    QLabel* plblCompleted = new QLabel("Выполнено", this);
+    plblCompleted->setStyleSheet("font-size: 15pt;");
+    pHLayoutLabels->addWidget(plblCompleted);
 
     QHBoxLayout* pHLayout = new QHBoxLayout();
     pMainLayout->addLayout(pHLayout);
 
 
     m_pwPending = new QListView(this);
+
 
     /*Добавляем виджет*/
     pHLayout->addWidget(m_pwPending);
@@ -40,6 +49,7 @@ CToDoList::CToDoList()
     m_pwCompleted = new QListView(this);
 
 
+
     /*Добавляем виджет*/
     pHLayout->addWidget(m_pwCompleted);
 
@@ -47,6 +57,38 @@ CToDoList::CToDoList()
     m_pwPending->setModel(new QStringListModel());
     m_pwCompleted->setModel(new QStringListModel());
 
+
+    /*Задаются цвет и формы таблички поставленных задач*/
+    m_pwPending->setStyleSheet
+    ("QListView { font-size: 20pt; font-weight: bold; }"
+     "QListView::item { background-color: #E74C3C; padding: 10%;"
+     "border: 1px solid #C0392B; }"
+     "QListView::item::hover { background-color: #C0392B }");
+
+    /*Задаются цвет и формы таблички выполненных задач*/
+    m_pwCompleted->setStyleSheet
+    ("QListView { font-size: 20pt; font-weight: bold; }"
+     "QListView::item { background-color: #2ECC71; padding: 10%;"
+     "border: 1px solid #27AE60; }"
+     "QListView::item::hover { background-color: #27AE60 }");
+
+
+    QToolBar* pToolBar = new QToolBar(this);
+    addToolBar(pToolBar);
+
+    /*Задаем триггер добавления новой задачи и обозначаем его картинкой*/
+    m_pActAdd = new QAction(this);
+    m_pActAdd->setIcon(QIcon(":/resources/add.png"));
+    connect(m_pActAdd, &QAction::triggered, this, &CToDoList::onAdd);
+
+    /*Задаем триггер удаления задачи и обозначаем его картинкой*/
+    m_pActRemove = new QAction(this);
+    m_pActRemove->setIcon(QIcon(":/resources/remove.png"));
+    connect(m_pActRemove, &QAction::triggered, this, &CToDoList::onRemove);
+
+    /*Добавляем действия*/
+    pToolBar->addAction(m_pActAdd);
+    pToolBar->addAction(m_pActRemove);
 }
 
 
